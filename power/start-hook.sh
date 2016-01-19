15c467271c9f730fa1e958b61d71c1b80d86e7e1
Marc Titinger <mtitinger@baylibre.com>
2016-01-19 12:03:29 +0100

    fix start-hook, use iio-capture
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- power/start-hook.sh
+++ power/start-hook.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-## FIXME: understand how to put a step in backgrounf on host
+## FIXME: understand how to put a step in background on host
 ##
 date +%s > capture-start-time
 
-#replace with iio_readdev
-sleep 100 & 
+dut=`echo $DEVICE | sed -e 's|dut\([0-9]*\)-.*|\1|g'`
 
-#fake dat file
-touch power.cat
+iio-capture -n lab-baylibre-acme.local iio:device$dut > $1/attachments/$dut_power_report.json.part
 
 echo $! > capture-pid
